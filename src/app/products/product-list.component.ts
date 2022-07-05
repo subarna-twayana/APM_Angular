b5648bdd2cd59da5c7ba6414e1d60d131a7c7f97
Subarna1 <subarnatwayana@gmail.com>
2022-07-05 22:24:45 +0545

    adding logics with directives

--- src/app/products/product-list.component.ts
+++ src/app/products/product-list.component.ts
@@ -6,5 +6,27 @@ import { Component } from "@angular/core";
 })
 
 export class ProductListComponent {
-
+    pageTitle: string = 'Product List';
+    products: any[] =[
+        {
+          "productId": 1,
+          "productName": "Garden Cart",
+          "productCode": "ASD-0123",
+          "releaseDate": "March 15, 2022",
+          "description": "rolling garden cart",
+          "price": 34.98,
+          "starRating": 4.2,
+          "imageUrl": "assets/images/garden_cart.png" 
+        },
+        {
+          "productId": 2,
+          "productName": "Hammer",
+          "productCode": "ASD-0003",
+          "releaseDate": "July 198, 2022",
+          "description": "rolling garden cart",
+          "price": 78.98,
+          "starRating": 4.8,
+          "imageUrl": "assets/images/hammer.png" 
+        }
+    ]
 }
