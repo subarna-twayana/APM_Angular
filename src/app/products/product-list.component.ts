5bfef8b0a23a66eab66bc0c733307f5723cf98df
Subarna1 <subarnatwayana@gmail.com>
2022-07-06 14:56:49 +0545

    one way data binding

--- src/app/products/product-list.component.ts
+++ src/app/products/product-list.component.ts
@@ -7,6 +7,9 @@ import { Component } from "@angular/core";
 
 export class ProductListComponent {
     pageTitle: string = 'Product List';
+    imageWidth: number = 50;
+    imageMargin: number = 2;
+    showImage: boolean =false;
     products: any[] =[
         {
           "productId": 1,
@@ -28,5 +31,10 @@ export class ProductListComponent {
           "starRating": 4.8,
           "imageUrl": "assets/images/hammer.png" 
         }
-    ]
+    ];
+
+  toggleImage(): void{
+    this.showImage = !this.showImage;
+  }
+
 }
